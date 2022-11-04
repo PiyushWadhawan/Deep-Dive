faed3c170590d21951c20c779304e1c865fc6556
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2022-11-05 03:05:02 +0530

    Updating footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../App.css'
+import Cards from '../components/Cards'
+import Footer from '../components/Footer'
 import IntroPage from '../components/IntroPage'
 
 
@@ -7,6 +9,8 @@ function Home() {
   return (
     <>
       <IntroPage/>
+      <Cards/>
+      <Footer/>
     </>
   )
 }
