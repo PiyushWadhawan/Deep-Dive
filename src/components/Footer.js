f2df90141a92a8e3a6e90efd60dbe43ae157f067
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2022-11-05 02:58:50 +0530

    added footer and card

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from './Button'
+import { GiSharkFin } from 'react-icons/gi'
+import './Footer.css'
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <section className="footer-subscription">
+            <p className="footer-subscription-heading">You have a question?</p>
+            <p className="footer-subscription-text">Contact us anytime</p>
+            <div className="input-areas">
+                <form >
+                    <input type="email" name="email" placeholder="Your Email" className="footer-input" />
+                    <Button buttonStyle='btn-outline'>Subscribe</Button>
+                </form>
+            </div>
+        </section>
+        <div className="footer-links">
+            <div className="footer-link-wrapper">
+                <div className="footer-link-items">
+                    <h2>About Us</h2>
+                    <Link to='/'>How it works</Link>
+                    <Link to='/'>Testimonials</Link>
+                    <Link to='/'>Careers</Link>
+                    <Link to='/'>Investors</Link>
+                    <Link to='/'>Terms of Service</Link>
+                </div>
+                <div className="footer-link-items">
+                    <h2>About Us</h2>
+                    <Link to='/'>How it works</Link>
+                    <Link to='/'>Testimonials</Link>
+                    <Link to='/'>Careers</Link>
+                    <Link to='/'>Investors</Link>
+                    <Link to='/'>Terms of Service</Link>
+                </div>
+            </div>
+        </div>
+        <section className="social-media">
+            <section className="social-media-wrap">
+                <div className="footer-logo">
+                    <Link to='/' className='social-logo'>
+                        DeepDive <GiSharkFin className='fa-typo3'/>
+                    </Link>
+                </div>
+                <small className="website-rights">DeepDive © 2022</small>
+                <div className="social-icons">
+                    <Link className='social-icon-link facebook' to='/' aria-label='Facebook'>
+                        <i className='fab fa-facebook-f'></i>
+                    </Link>
+                    <Link className='social-icon-link facebook' to='/' aria-label='Facebook'>
+                        <i className='fab fa-facebook-f'></i>
+                    </Link>
+                    <Link className='social-icon-link facebook' to='/' aria-label='Facebook'>
+                        <i className='fab fa-facebook-f'></i>
+                    </Link>
+                </div>
+            </section>
+        </section>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -5,16 +5,17 @@ import './IntroPage.css'
 function IntroPage() {
   return (
       <>
-      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <video src='/videos/video-6.mp4' autoPlay loop muted />
       <div className='intro-container'>
-        <h1>Let's conquer the Depths</h1>
+        <h1>Let's Conquer The Depths</h1>
         <p>What are you waiting for?</p>
+        <p>Find the career that is best for you</p>
         <div className="intro-btns">
             <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
-                GET STARTED
+                HOW IT WORKS?
             </Button>
             <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
-                REGISTER
+                TAKE THE TEST
             </Button>
         </div>
     </div>
