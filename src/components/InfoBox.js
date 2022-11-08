60c326e5ac2fef37fb99d945fba5e26d44af221e
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2022-11-09 04:13:09 +0530

    Responsive infoBox and spacing

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './InfoBox.css'
+import { useState, useEffect } from 'react'
 
-function InfoBox({ heading, points, format, image }) {
+function InfoBox({ heading, points, format, image, altimage }) {
+
+  const [isDesktop, setDesktop] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 1024) 
+      setDesktop(true);
+    else 
+      setDesktop(false);
+    
+
+    const updateMedia = () => {
+      if (window.innerWidth > 1024) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
+  }, []);
+
   return (
     <>
-      <div className={format === 'normal'? 'info-container' : 'info-container mirrored'}>
+      <div className={format === 'normal'? 'info-container' : 'info-container mirrored'} style={{background: isDesktop === true ? "":`url(${altimage}) center center/cover no-repeat`}}>
         
         <div className="pic" >
             <img src={image} alt="pictures"/>
