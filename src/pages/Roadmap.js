7c62271db7f3946554e93b29ce916261883fb667
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2022-12-04 23:09:05 +0530

    Removed Errors

--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -11,11 +11,11 @@ function Roadmap() {
     <div style={{ 'paddingTop': "4rem", 'paddingBottom': "6rem", "backgroundColor": "black", "color": "aqua" }}>
       <h1 style={{"paddingBottom": "1rem"}}>Roadmap for {heading}</h1>
       {
-        RoadmapData[career_id].map((item) => <RoadMapContainer point = {item.point} resources={item.resources}/>)
+        RoadmapData[career_id].map((item) => <RoadMapContainer key={item.point}  point = {item.point} resources={item.resources}/>)
       }
-      <div style={{"width": "0", "height": "4rem","border-left": "2px solid white", "border-right": "2px solid white", "border-top": "", "margin": "0 auto"}}>
+      <div style={{"width": "0", "height": "4rem","borderLeft": "2px solid white", "borderRight": "2px solid white", "borderTop": "", "margin": "0 auto"}}>
       </div>
-      <div className='down-arrow' style={{"width": "0", "height": "0","border-left": "25px solid transparent","border-right": "25px solid transparent", "border-top": "50px solid white", "margin": "0 auto"}}>
+      <div className='down-arrow' style={{"width": "0", "height": "0","borderLeft": "25px solid transparent","borderRight": "25px solid transparent", "borderTop": "50px solid white", "margin": "0 auto"}}>
       </div>
     </div>
   )
